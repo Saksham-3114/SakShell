abd81f90ac51355e3b55b258e97787bf8ae6e22f
Saksham Singhal <116955907+Saksham-3114@users.noreply.github.com>
2024-12-22 12:20:20 +0530

    execProgInQuote

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@ enum class TypeEnum {
   PWD,
   CD,
 };
+
 struct TypeDscr {
   TypeEnum type;
   std::string_view typeStr;
@@ -25,12 +26,7 @@ std::array<TypeDscr, 5> types = {{
   { TypeEnum::PWD,  "pwd"  },
   { TypeEnum::CD,   "cd"   },
 }};
-std::string getWord(const std::string& input, size_t& offset) {
-  std::string result = "";
-  auto index = input.find(' ', offset);
-  if (index != std::string::npos) {
-    result = input.substr(offset, index);
-    offset = index + 1;
+
 void removeSymbFromStart(std::string& str, char c) {
   size_t start = 0;
   while (str[start] == c && start < str.size()) {
@@ -51,9 +47,7 @@ void removeSymbFromEnd(std::string& str, char c) {
   if (end > 0) {
     str = str.substr(0, end+1);
   } else {
-    result = input;
-    offset = input.size();
-    str = "";
+        str = "";
   }
 }
 std::string getWord(const std::string& input, size_t& offset) {
@@ -68,31 +62,44 @@ std::string getWord(const std::string& input, size_t& offset) {
     offset++;
   }
   return result;
-}
-Expand 7 lines
+} 
+
+TypeEnum getType(const std::string& word) {
+  for (auto& t : types) {
+    if (t.typeStr == word) {
+      return t.type;
+    }
+  }
   return TypeEnum::NOT_FOUND;
 }
-bool checkExit(const std::string& str, size_t offset) {
-  return (offset == str.size()-1 && str[offset] == '0');
+
 bool checkExit(const std::string& input) {
   return input == "exit 0";
 }
 void updateBackSlash(std::string& str, int state) {
-Expand 22 lines
+  int j = 0;
+  // std::cout << "state = " << state << ", str = " << str << std::endl;
+  if (state == 0) {
+    if (str == "\\") {
+      str = " ";
+      return;
+    } else if (*str.rbegin() == '\\') {
+      str += ' ';
+    }
+    bool flag = false;
+    for (int i = 0; i < str.size(); i++) {
+      if (i > 0 && str[i] != '\\' ) {
+        j++;
+      }
+      if (i > j && str[i] != '\\' ) {
+        str[j] = str[i];
+      }
+    }
+    // std::cout << "j = " << j << std::endl;
     if (j > 0) {
       str = str.substr(0, j+1);
     }
-  } else if (state == 1) {
-    // for (int i = 0; i < str.size(); i++) {
-      // if (!(i > 0 && str[i] == '\\' && str[i-1] == '\\')) {
-      //   j++;
-      // }
-      // if (i > j) {
-      //   str[j] = str[i];
-      // }
-    // }
-    // str = str.substr(0, j+1);
-  } else if (state == 2) {
+    } else if (state == 2) {
     for (int i = 0; i < str.size(); i++) {      
       bool isSpecSymb = str[i] == '\\' && 
         (i < str.size() && (str[i+1] == '\\' || str[i+1] == '$' || str[i+1] == '\"') ) ;
@@ -107,7 +114,6 @@ Expand 22 lines
   }
   
 }
-void tokenize(std::string& result, const std::string& input) {
 void tokenize(std::string& result, const std::string& input, size_t offset) {
   std::string token = "";
   result = "";
@@ -125,7 +131,6 @@ void tokenize(std::string& result, const std::string& input, size_t offset) {
   };
   int state = 0;
   char prevC = 'x';
-  for (int i = 0; i < input.size(); i++) {
   for (size_t i = offset; i < input.size(); i++) {
     switch (state) {
       case 0: {
@@ -146,7 +151,7 @@ void tokenize(std::string& result, const std::string& input, size_t offset) {
         }
       }
       break;
-      case 1: {
+       case 1: {
         if (input[i] == '\'') {
           updateBackSlash(token, state);
           addStrToResult(token);
@@ -157,7 +162,7 @@ void tokenize(std::string& result, const std::string& input, size_t offset) {
         }
       }
       break;
-      case 2: {
+       case 2: {
         if (input[i] == '"' && prevC != '\\') {
           updateBackSlash(token, state);
           addStrToResult(token);
@@ -181,11 +186,9 @@ void tokenize(std::string& result, const std::string& input, size_t offset) {
     token.clear();
   }
 }
-void doEcho(const std::string& input) {
 void doEcho(const std::string& input, size_t offset) {
   skipSymbol(input, offset, ' ');
   std::string result;
-  tokenize(result, input);
   tokenize(result, input, offset);
   std::cout << result;
   std::cout << std::endl;
@@ -199,7 +202,6 @@ bool findFilePath(std::string& filePath, const std::string& fileName, const std:
   }
   return false;
 }
-void doType(const std::string& word, const std::unordered_set<std::string>& path) {
 void doType(const std::string& input, size_t offset, const std::unordered_set<std::string>& path) {
   skipSymbol(input, offset, ' ');
   std::string word = input.substr(offset);
@@ -217,30 +219,17 @@ void doType(const std::string& input, size_t offset, const std::unordered_set<st
 void doPwd() {
   std::cout << std::filesystem::current_path().string() << std::endl;
 }
-void doCd(const std::string& path) {
-  const char* env_p = std::getenv("HOME");
-  std::string homePath = "";
-  int i = 0;
-  while (env_p[i] != '\0') {
-    homePath += env_p[i++];
-  }
 void doCd(const std::string& input, size_t offset) {
   skipSymbol(input, offset, ' ');
-  std::string pathUpd = path;
-  if (path[0] == '~') {
-    pathUpd = homePath + path.substr(1);
   std::string path;
   if (input[offset] == '~') {
     path = std::getenv("HOME") + input.substr(offset+1);
   } else {
     path = input.substr(offset);
   }
-  if (std::filesystem::exists(pathUpd)) {
-    std::filesystem::current_path(pathUpd);
   if (std::filesystem::exists(path)) {
     std::filesystem::current_path(path);
   } else {
-    std::cout << "cd: " << pathUpd << ": No such file or directory" << std::endl;
     std::cout << "cd: " << path << ": No such file or directory" << std::endl;
   }
 }
@@ -274,8 +263,6 @@ std::string doExecute(const std::string& filePath) {
   // std::cout << "filePath = " << filePath << ", result = " << result << std::endl;
   return result;
 }
-bool tryRunExec(const std::string& word, const std::string& str, 
-  size_t offset, const std::unordered_set<std::string>& path) {
 bool checkExeWith(const std::string& word, const std::string& input, size_t& offset){
   if (word == "exe" || word == "'exe" || word == "\"exe") {
     while (input[offset] != '/') {
@@ -288,33 +275,13 @@ bool checkExeWith(const std::string& word, const std::string& input, size_t& off
 bool tryRunCommand(const std::string& word, const std::string& str,
   const std::unordered_set<std::string>& path) {
   std::string filePath;
+
   if (findFilePath(filePath, word, path)) {
-    // std::cout << "command = " << (filePath + str.substr(offset-1)) << std::endl;
-    std::cout << doExecute(filePath + str.substr(offset-1));
-    // std::cout << "command = " << (filePath + str) << std::endl;
     std::cout << doExecute(filePath + " " + str);
     return true;
   }
   return false;
 }
-// std::string getFileContent(const std::string filePath) {
-//   std::ifstream f(filePath, std::ios_base::in);
-//   std::string tmp, result;
-//   std::stringstream buffer;
-//   buffer << f.rdbuf();
-//   while (std::getline(buffer, tmp)) {
-//     result += tmp;
-//   }
-//   return result;
-// }
-// void doCat(const std::string& input) {
-//   std::list<std::string> tokens;
-//   tokenize(tokens, input);
-//   for (auto& t : tokens) {
-//     std::cout << getFileContent(t);
-//   }
-//   std::cout << std::endl;
-// }
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
@@ -329,49 +296,33 @@ int main() {
   bool error;
   bool exit = false;
   size_t offset;
-  std::string word;
   std::string command;
   while (true) {
     std::getline(std::cin, input);
     offset = 0;
     error = false;
-    word = getWord(input, offset);
     command = getWord(input, offset);
-    // std::cout << "command = '"  << command <<"', offset = " << offset << std::endl;
-    switch (getType(word)) {
     switch (getType(command)) {
       case TypeEnum::EXIT: {
-        if (checkExit(input, offset)) {
-          exit = true;
-        } else {
-          error = true;
-        }
         exit = checkExit(input);
       }
       break;
       case TypeEnum::ECHO: {
-        doEcho(input.substr(offset));
         doEcho(input, offset);
       }
       break;
       case TypeEnum::TYPE: {
-        doType(input.substr(offset), path);
-        doType(input, offset, path);
-      }
+        }
       break;
       case TypeEnum::PWD: {
         doPwd();
       }
       break;
       case TypeEnum::CD: {
-        doCd(input.substr(offset));
         doCd(input, offset);
       }
       break;
- 
       case TypeEnum::NOT_FOUND: {
-        if (!tryRunExec(word, input, offset, path)) {
-          error = true;
         if (command != "cat" && checkExeWith(command, input, offset)) {
           command = "cat";
         }
@@ -380,8 +331,7 @@ int main() {
       break;
     }
     if (error) {
-      doCommandNotFound(word);
-      doCommandNotFound(command);
+       doCommandNotFound(command);
     }
     if (exit) {
       break;
